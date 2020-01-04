686488055102cec76122fc2107659195b798b81a
Evan Greer <etgreer91@gmail.com>
2020-01-04 06:11:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game
   #
   user_guess = gets.chomp.to_s
   
-  if user_guess == 'exit'
+  if user_guess == 
     puts "Goodbye!"
-  elsif user_guess == number
+  elsif user_guess == 
     puts "You guessed the correct number!"
   elsif user_guess != number
     puts "Sorry! The computer guessed #{number}."
